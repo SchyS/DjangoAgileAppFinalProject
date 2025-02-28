97447373123ed20848a8dfab5c2ee68723137f1a
Schylar Srey <schylar.srey@uhsp.edu>
2025-02-27 23:06:35 -0600

    Debug fixes in urls.py

--- web_project/urls.py
+++ web_project/urls.py
@@ -1,21 +1,22 @@
-#Import modules
-from django.contrib import admin #Django admin module
-from django.urls import path #URL routing
-from authentication.views import * #Import views from the authentcation app
-from django.conf import settings #Application settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns #Static files serving
+# Import necessary modules
+from django.contrib import admin  # Django admin module
+from django.urls import path       # URL routing
+from users.views import *  # Import views from the authentication app
+from django.conf import settings # Application settings
+from django.conf.urls.static import static  # Import static for serving media files
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns  # Static files serving
 
-#Define url paths
+# Define URL patterns
 urlpatterns = [
-    path('admin/', admin.site.urls), #Home Page
-    path("admin/", admin.site.urls), #Admin interface
-    path('login/', login_page, name='login_page'), #Login Page
-    path('register/', register_page, name='register'), #Registration page
+    path('home/', home, name="recipes"),      # Home page
+    path("admin/", admin.site.urls),          # Admin interface
+    path('login/', login_page, name='login_page'),    # Login page
+    path('register/', register_page, name='register'),  # Registration page
 ]
 
-#Serve media files if DEBUG is Trie (development mode)
+# Serve media files if DEBUG is True (development mode)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-#Serve static files using staticfiles_urlpatterns
-urlpatterns += staticfiles_urlpatterns
+# Serve static files using staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
